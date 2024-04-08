1c0473a65935831103e4f028b43ed841c176399e
tejasXR <tejasshroff24@gmail.com>
2024-04-08 10:38:28 -0400

    Simplifying bound generation process

--- Assets/_App/Scripts/Garden/SeedSpawner.cs
+++ Assets/_App/Scripts/Garden/SeedSpawner.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 public class SeedSpawner : MonoBehaviour
 {
     [SerializeField] private SeedController seedPrefab;
+    [Space(5)]
     [SerializeField] private SpawnSurfacesEnum spawnSurfaces;
-    
+    [SerializeField] private SpawnSurfaceAccessibilityEnum spawnSurfaceAccessibility;
     [Space(5)]
     [SerializeField] private int maxSeedsToSpawn = 10;
     [SerializeField] private float maxHeightToSpawn = 1.5F;
-
+    
+    private const float SurfaceClearingDistance = .75F;  // The clearance distance required in front of the surface in order for it to be considered a valid spawn position
     private Pooler<SeedController> _seedPooler;
 
     private enum SpawnSurfacesEnum
@@ -18,43 +20,44 @@ public class SeedSpawner : MonoBehaviour
         AllSurfaces
     }
 
+    private enum SpawnSurfaceAccessibilityEnum
+    {
+        SpawnNearKeyWall,
+        SpawnNearAllWalls
+    }
+
     public void Initialize()
     {
         _seedPooler = new Pooler<SeedController>();
         _seedPooler.Initialize(OnPoolerBorrowedItem, maxSeedsToSpawn);
         SpawnSeeds();
     }
-
-    private Bounds CreateSpawnBounds()
-    {
-        Bounds roomBounds = MRUK.Instance.GetCurrentRoom().GetRoomBounds();
-        
-        // Make the center of the room half the spawn height
-        var customCenter = roomBounds.center;
-        customCenter.y = maxHeightToSpawn / 2;
-        roomBounds.center = customCenter;
-        
-        // Modify the size of the bounds to reflect max spawn height 
-        var customSize = roomBounds.size;
-        customSize.y = maxHeightToSpawn;
-        roomBounds.size = customSize;
-
-        return roomBounds;
-    }
-
+    
     private void SpawnSeeds()
     {
+        // Generate bound information
+        var entireRoomBounds = MRUK.Instance.GetCurrentRoom().GetRoomBounds();
+        var keyWallAnchor = MRUK.Instance.GetCurrentRoom().GetKeyWall(out Vector2 keyWallScale);
+        var keyWallCenter = keyWallAnchor.GetAnchorCenter();
+        var keyWallBounds = new Bounds(keyWallCenter, keyWallScale);  
+        
+        var spawnBounds = spawnSurfaceAccessibility == SpawnSurfaceAccessibilityEnum.SpawnNearAllWalls 
+            ? GenerateBoundsFromReference(entireRoomBounds) : GenerateBoundsFromReference(keyWallBounds);
+        
         // Get spawn position information
-        var spawnBounds = CreateSpawnBounds();
         var getSpawnPositions = SpawnUtil.GetSpawnPositions
         (
+            // TEJAS: I original thought that the objectBounds arg was the bounds where to place objects, not the physical bouding dimensions of an object :')
+            // Leaving this comment in because my intent was to bound the spawning of objects by specific bounds, which we may do later in development
+            // objectBounds: spawnBounds,
+            
             objectBounds: spawnBounds,
             positionCount: maxSeedsToSpawn,
-            spawnLocation: FindSpawnPositions.SpawnLocation.VerticalSurfaces,
+            spawnLocation: FindSpawnPositions.SpawnLocation.AnySurface,
             labels: spawnSurfaces == SpawnSurfacesEnum.Walls ? MRUKAnchor.SceneLabels.WALL_FACE | MRUKAnchor.SceneLabels.WINDOW_FRAME | MRUKAnchor.SceneLabels.WALL_ART : ~(MRUKAnchor.SceneLabels)0,
-            surfaceClearanceDistance: .5F
+            surfaceClearanceDistance: SurfaceClearingDistance
         );
-
+      
         // Generate pooled objects
         foreach (var tupleVector3Quaternion in getSpawnPositions)
         {
@@ -62,6 +65,22 @@ public class SeedSpawner : MonoBehaviour
             pooledSeed.transform.SetParent(transform);
             pooledSeed.transform.SetPositionAndRotation(tupleVector3Quaternion.Item1, tupleVector3Quaternion.Item2); 
         }
+        
+    }
+
+    private Bounds GenerateBoundsFromReference(Bounds referenceBound)
+    {
+        // Make the center of the room half the spawn height
+        var customCenter = referenceBound.center;
+        customCenter.y = maxHeightToSpawn / 2;
+        referenceBound.center = customCenter;
+        
+        // Modify the size of the bounds to reflect max spawn height 
+        var customSize = referenceBound.size;
+        customSize.y = maxHeightToSpawn;
+        referenceBound.size = customSize;
+        
+        return referenceBound;
     }
 
     private SeedController OnPoolerBorrowedItem(int numberOfTotalItemsBorrowed)
