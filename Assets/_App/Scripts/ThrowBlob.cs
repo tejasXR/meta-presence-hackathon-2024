0e58508c7656e961375887959fa64d142f5c1cd9
Simran Jagnik <48251642+jagniksimran@users.noreply.github.com>
2024-04-11 02:17:45 -0700

    Basic hand interaction
     Trying to throw the blob

---
+++ Assets/_App/Scripts/ThrowBlob.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBlob : MonoBehaviour
+{
+    public Vector3 throwDirection = Vector3.forward; 
+    public float throwForce = 10f;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update  public float throwForce = 10f;
+private void OnTriggerExit(Collider other)
+    {
+    ThrowObject(rb,throwDirection);
+    }
+       public void ThrowObject(Rigidbody rb, Vector3 throwDirection)
+    {
+        rb.AddForce(throwDirection * throwForce, ForceMode.Impulse);
+    }
+}
+
