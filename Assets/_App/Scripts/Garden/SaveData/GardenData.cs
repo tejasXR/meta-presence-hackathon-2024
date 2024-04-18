240977386ac2c88f30b1cc59ec7dd87226349ddc
tejasXR <tejasshroff24@gmail.com>
2024-04-18 19:09:17 -0400

    Moving PlantData struct into its own file

--- Assets/_App/Scripts/Garden/SaveData/GardenData.cs
+++ Assets/_App/Scripts/Garden/SaveData/GardenData.cs
@@ -5,15 +5,6 @@ using System.Collections.Generic;
 [Serializable]
 public class GardenData
 {
-    [Serializable]
-    public struct PlantData
-    {
-        public Guid Uuid;
-        public string Type;
-        public DateTime CreatedAt;
-        public float GrowValue;
-    }
-
     public Dictionary<Guid, PlantData> Map = new();
     public string DateTimeOfLastVisit;
     
