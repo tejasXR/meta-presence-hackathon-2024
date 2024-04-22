b9beb84cccdab6bb5d8a03abdc6ba5ba5744452e
yolanda <yolanda@runnningwild.io>
2024-04-22 21:55:38 +0100

    Rough version of building vs. gazing mode detection

--- Assets/_App/Scripts/GameManager.cs
+++ Assets/_App/Scripts/GameManager.cs
@@ -7,31 +7,74 @@ public class GameManager : Singleton<GameManager>
 {
     public GameMode CurrentGameMode = GameMode.Building;
 
+    [Header("Gazing Mode Config")]
+    [SerializeField] private float _gazingPositionThreshold = 0.01f;
+    [SerializeField] private float _gazingTimeThreshold = 3f;
+
+    [Space]
     [SerializeField] private PassthroughController _passthroughController;
 
     [Space]
     public UnityEvent<GameMode> OnGameModeChanged;
 
+    private Transform _cameraTransform;
+    private Vector3 _lastCameraPosition;
+    private float _gazingTimer = 0f;
+
+#if UNITY_EDITOR
     private GameMode _lastValidatedGameMode = GameMode.Building;
+#endif
 
     void Start()
     {
-        GameModeChanged();
+        _cameraTransform = Camera.main.transform;
+        _lastCameraPosition = _cameraTransform.position;
+
+        GameModeChanged(CurrentGameMode);
     }
 
+    void Update()
+    {
+        float deltaPos = Vector3.Distance(_lastCameraPosition, _cameraTransform.position);
+
+        if (deltaPos < _gazingPositionThreshold)
+        {
+            _gazingTimer += Time.deltaTime;
+            if (_gazingTimer >= _gazingTimeThreshold && CurrentGameMode != GameMode.Gazing)
+            {
+                GameModeChanged(GameMode.Gazing);
+            }
+        }
+        else
+        {
+            _gazingTimer = 0f;
+            if (CurrentGameMode != GameMode.Building)
+            {
+                GameModeChanged(GameMode.Building);
+            }
+        }
+
+        _lastCameraPosition = _cameraTransform.position;
+    }
+
+#if UNITY_EDITOR
     void OnValidate()
     {
         if (_lastValidatedGameMode != CurrentGameMode)
         {
-            Debug.Log($"[{nameof(GameManager)}] {nameof(OnValidate)}: {nameof(GameMode)} changed: previous={_lastValidatedGameMode}, current={CurrentGameMode}");
+            Debug.Log(message: $"[{nameof(GameManager)}] {nameof(OnValidate)}: {nameof(GameMode)} changed: previous={_lastValidatedGameMode}, current={CurrentGameMode}");
             _lastValidatedGameMode = CurrentGameMode;
-            GameModeChanged();
+
+            GameModeChanged(CurrentGameMode);
         }
     }
+#endif
 
-    private void GameModeChanged()
+    private void GameModeChanged(GameMode mode)
     {
-        _passthroughController.SetLut((int)CurrentGameMode);
-        OnGameModeChanged?.Invoke(CurrentGameMode);
+        CurrentGameMode = mode;
+
+        _passthroughController.SetLut((int)mode);
+        OnGameModeChanged?.Invoke(mode);
     }
 }
