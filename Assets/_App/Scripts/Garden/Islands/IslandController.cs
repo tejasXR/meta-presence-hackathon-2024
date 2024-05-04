86ba6c9d573c1a6708e4c96e0495d81cf97fdf38
yolanda <yolanda@runnningwild.io>
2024-05-04 11:17:42 +0100

    Nit: clean up

--- Assets/_App/Scripts/Garden/Islands/IslandController.cs
+++ Assets/_App/Scripts/Garden/Islands/IslandController.cs
@@ -37,15 +37,12 @@ public class IslandController : MonoBehaviour
             Vector3 noise = Vector3.zero;
             if (_rumble)
             {
-                float distanceFromOrigin = Mathf.Abs(_meshRenderer.transform.localPosition.y - _originalLocalPosition.y);
-                distanceFromOrigin = Mathf.Clamp01(distanceFromOrigin);
-                float xDirection = Random.Range(-distanceFromOrigin, distanceFromOrigin);
-                float zDirection = Random.Range(-distanceFromOrigin, distanceFromOrigin);
+                float intensity = Mathf.Clamp01(Mathf.Abs(_meshRenderer.transform.localPosition.y - _originalLocalPosition.y)); // Intensity reduces as the island gets closer to its origin.
                 float xNoise = Mathf.PerlinNoise(_rumbleSpeed * Time.time, 0);
                 float zNoise = Mathf.PerlinNoise(0f, _rumbleSpeed * Time.time);
 
-                noise.x = xDirection * xNoise;
-                noise.z = zDirection * zNoise;
+                noise.x = Random.Range(-intensity, intensity) * xNoise;
+                noise.z = Random.Range(-intensity, intensity) * zNoise;
             }
             _meshRenderer.transform.localPosition = Vector3.Lerp(_meshRenderer.transform.localPosition, new(noise.x * _rumbleIntensity, _originalLocalPosition.y, noise.z * _rumbleIntensity), _emergeSpeed * Time.deltaTime);
 
