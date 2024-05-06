0f7fe01c6af83952f7498b355a097b137e543d6d
tejasXR <tejasshroff24@gmail.com>
2024-05-06 18:02:46 -0400

    Adding seed forming particle to plants

--- Assets/_App/Scripts/VFX/PlantReadyVfx.cs
+++ Assets/_App/Scripts/VFX/PlantReadyVfx.cs
@@ -1,39 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlantReadyVfx : MonoBehaviour
 {
+    
     [SerializeField] private ParticleSystem plantReadyParticles;
+    [SerializeField] private ParticleSystem seedFormingParticles;
     [SerializeField] private Color defaultColor;
     [SerializeField] private Color hoverColor;
 
-    public void StartParticles()
+    private PlantController _plant;
+
+    private void Awake()
+    {
+        _plant = GetComponentInParent<PlantController>();
+        _plant.PlantReadyToBeHarvested.AddListener(OnPlantReadyToBeHarvested);
+        _plant.PlantChargingUp.AddListener(OnPlantCharging);
+        _plant.PlantChargingDown.AddListener(OnPlantDefault);
+        _plant.PlantGlowingBeforeSeedSpawn.AddListener(OnPlantGlowing);
+        _plant.SeedSpawningTriggered.AddListener(OnSeedSpawned);
+    }
+
+    private void OnDestroy()
+    {
+        if (_plant)
+        {
+            _plant.PlantReadyToBeHarvested.RemoveListener(OnPlantReadyToBeHarvested);
+            _plant.PlantChargingUp.RemoveListener(OnPlantCharging);
+            _plant.PlantChargingDown.RemoveListener(OnPlantDefault);
+            _plant.PlantGlowingBeforeSeedSpawn.RemoveListener(OnPlantGlowing);
+            _plant.SeedSpawningTriggered.RemoveListener(OnSeedSpawned);
+        }
+    }
+
+
+    private void OnPlantReadyToBeHarvested()
     {
         if (!plantReadyParticles.isPlaying)
             plantReadyParticles.Play();
     }
 
-    public void StopParticles()
+    private void OnPlantDefault()
     {
-        if (plantReadyParticles.isPlaying)
-            plantReadyParticles.Stop();
-    } 
-
-    public void DefaultState()
-    {
-        ChangeStartColor(defaultColor);
+        ChangePlantReadyParticlesStartColor(defaultColor);
     }
     
-    public void HoverState()
+    private void OnPlantCharging()
     {
-        ChangeStartColor(hoverColor);
+        ChangePlantReadyParticlesStartColor(hoverColor);
     }
 
-    private void ChangeStartColor(Color startColor)
+    private void OnPlantGlowing()
+    {
+        seedFormingParticles.Play();
+    }
+
+    private void OnSeedSpawned(PlantController plantController)
+    {
+        seedFormingParticles.Stop();
+        plantReadyParticles.Stop();
+    }
+
+    private void ChangePlantReadyParticlesStartColor(Color startColor)
     {
         var main = plantReadyParticles.main;
         main.startColor = startColor;
     }
-
 }
